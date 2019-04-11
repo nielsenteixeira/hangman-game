f14702c42dc8c03e975aa23cbb32520653e3eb43
Nielsen Costa Teixeira <nielsenteixeira@gmail.com>
2019-04-11 11:20:43 -0300

    add created_at and modified_at

--- src/main/java/com/nielsen/hangman/game/model/BaseEntity.java
+++ src/main/java/com/nielsen/hangman/game/model/BaseEntity.java
@@ -1,22 +1,42 @@
 package com.nielsen.hangman.game.model;
 
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.MappedSuperclass;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.util.Date;
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    //@JsonIgnore
     private Long id;
+
+    @JsonIgnore
+    @CreatedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name="created_at", updatable = false)
+    private Date createdAt;
+
+    @JsonIgnore
+    @LastModifiedDate
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date modifiedAt;
 
     public BaseEntity() {
     }
 
     public Long getId() {
         return id;
+    }
+
+    public ZonedDateTime getModifiedAt() {
+        return ZonedDateTime.ofInstant(modifiedAt.toInstant(), ZoneId.systemDefault());
     }
 }
